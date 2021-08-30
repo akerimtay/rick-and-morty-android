46eb7123a6414459ee22b16ae9bd833529554d82
Kerimtay Askar <46015854+akerimtay@users.noreply.github.com>
2021-08-31 00:00:58 +0600

    refactor base response

--- data/src/main/java/com/akerimtay/data/remote/gateway/CharacterRemoteGateway.kt
+++ data/src/main/java/com/akerimtay/data/remote/gateway/CharacterRemoteGateway.kt
@@ -1,11 +1,8 @@
 package com.akerimtay.data.remote.gateway
 
-import com.akerimtay.common.BaseError
 import com.akerimtay.data.mapper.CharacterMapper
-import com.akerimtay.data.remote.base.BaseResponse
-import com.akerimtay.data.remote.base.NetworkResponse
 import com.akerimtay.data.remote.service.CharacterService
-import com.akerimtay.domain.error.CharacterError
+import com.akerimtay.data.remote.utill.handleResponse
 import com.akerimtay.domain.model.Character
 import com.akerimtay.domain.model.CharacterStatus
 import com.akerimtay.domain.model.Characters
@@ -27,22 +24,13 @@ class CharacterRemoteGateway @Inject constructor(
             status = status,
             gender = gender
         )
-        val response = extractResponseData(networkResponse)
+        val response = networkResponse.handleResponse()
         return CharacterMapper.fromNetwork(response)
     }
 
     suspend fun getCharacterById(characterId: Int): Character {
         val networkResponse = characterService.getCharacterAsync(characterId)
-        val response = extractResponseData(networkResponse)
+        val response = networkResponse.handleResponse()
         return CharacterMapper.fromNetwork(response)
-    }
-
-    private fun <T : Any> extractResponseData(response: BaseResponse<T>): T {
-        return when (response) {
-            is NetworkResponse.Success -> response.body
-            is NetworkResponse.ApiError -> throw CharacterError.EndpointError(response.body.errorMessage)
-            is NetworkResponse.NetworkError -> throw BaseError.NetworkError
-            is NetworkResponse.UnknownError -> throw BaseError.UnknownError
-        }
     }
 }
--- domain/src/main/java/com/akerimtay/domain/error/CharacterError.kt
+++
@@ -1,9 +0,0 @@
-package com.akerimtay.domain.error
-
-import com.akerimtay.common.BaseError
-
-sealed class CharacterError(
-    override val message: String? = null
-) : BaseError() {
-    class EndpointError(message: String?) : CharacterError(message)
-}
