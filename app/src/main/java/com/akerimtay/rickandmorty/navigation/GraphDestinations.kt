2d2601c9ca4d703cd766c0879500fab1d08df9a7
Kerimtay Askar <46015854+akerimtay@users.noreply.github.com>
2021-09-25 10:29:03 +0600

    Fixes

--- app/src/main/java/com/akerimtay/rickandmorty/navigation/GraphDestinations.kt
+++ app/src/main/java/com/akerimtay/rickandmorty/navigation/GraphDestinations.kt
@@ -3,11 +3,12 @@ package com.akerimtay.rickandmorty.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.akerimtay.rickandmorty.characterDetail.ui.CharacterDetailScreen
 import com.akerimtay.rickandmorty.navigation.destination.CharacterDetailsDestination
 import com.akerimtay.rickandmorty.navigation.navigator.NavigationDestination
 
 private val composableDestinations: Map<NavigationDestination, @Composable () -> Unit> = mapOf(
-    CharacterDetailsDestination to { com.akerimtay.rickandmorty.characterDetail.ui.CharacterDetailScreen() },
+    CharacterDetailsDestination to { CharacterDetailScreen() },
 )
 
 fun NavGraphBuilder.addComposableDestinations() {
